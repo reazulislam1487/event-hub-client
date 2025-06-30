1b6d78ff7cfc8a62ed6d9e22f9062f21cf3c010a
Reazul Islam Reaz <reazulislam1487@gmail.com>
2025-07-01 01:18:40 +0600

    fixed navbar

--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -1,172 +1,20 @@
-// import React, { useContext } from "react";
-
-// import { NavLink, useNavigate } from "react-router";
-// import { AuthContext } from "../../Contexts/AuthContext";
-// import Swal from "sweetalert2";
-
-// const Navbar = () => {
-//   const { user, setUser } = useContext(AuthContext);
-//   const navigate = useNavigate();
-//   const userPic = "https://www.w3schools.com/howto/img_avatar.png"; // Default user picture
-
-//   const handleLogout = () => {
-//     Swal.fire({
-//       title: "Are you sure?",
-//       text: "You will be logged out!",
-//       icon: "warning",
-//       showCancelButton: true,
-//       confirmButtonText: "Yes, logout!",
-//       confirmButtonColor: "#2563EB", // blue
-//       cancelButtonColor: "#6B7280", // gray
-//     }).then((result) => {
-//       if (result.isConfirmed) {
-//         setUser(null);
-//         localStorage.removeItem("user", user);
-//       }
-//     });
-//   };
-//   console.log(user);
-
-//   const links = (
-//     <>
-//       <li>
-//         <NavLink
-//           to="/"
-//           className={({ isActive }) =>
-//             isActive
-//               ? "text-blue-600 font-semibold border-b-2 border-blue-600"
-//               : "hover:text-blue-500 transition"
-//           }
-//         >
-//           Home
-//         </NavLink>
-//       </li>
-//       <li>
-//         <NavLink
-//           to="/marathons"
-//           className={({ isActive }) =>
-//             isActive
-//               ? "text-blue-600 font-semibold border-b-2 border-blue-600"
-//               : "hover:text-blue-500 transition"
-//           }
-//         >
-//           Events
-//         </NavLink>
-//       </li>
-//       {user && (
-//         <li>
-//           <NavLink
-//             to="/dashboard"
-//             className={({ isActive }) =>
-//               isActive
-//                 ? "text-blue-600 font-semibold border-b-2 border-blue-600"
-//                 : "hover:text-blue-500 transition"
-//             }
-//           >
-//             Dashboard
-//           </NavLink>
-//         </li>
-//       )}
-//     </>
-//   );
-
-//   return (
-//     <nav className="navbar bg-gradient-to-b from-blue-100 via-white to-blue-50 shadow-md p-0 pr-2 md:px-6 md:py-3 sticky top-0 z-50">
-//       <div className="navbar-start flex items-center gap-4">
-//         <div className="dropdown ">
-//           <label
-//             tabIndex={0}
-//             className="btn btn-ghost lg:hidden p-2 rounded-md hover:bg-blue-100 focus:bg-blue-100 focus:outline-none"
-//           >
-//             <svg
-//               xmlns="http://www.w3.org/2000/svg"
-//               className="h-6 w-6 text-blue-600"
-//               fill="none"
-//               viewBox="0 0 24 24"
-//               stroke="currentColor"
-//               strokeWidth={2}
-//             >
-//               <path
-//                 strokeLinecap="round"
-//                 strokeLinejoin="round"
-//                 d="M4 6h16M4 12h8m-8 6h16"
-//               />
-//             </svg>
-//           </label>
-//           <ul
-//             tabIndex={0}
-//             className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-white rounded-box w-52"
-//           >
-//             {links}
-//           </ul>
-//         </div>
-//         <NavLink
-//           to="/"
-//           className="hidden sm:flex text-2xl font-extrabold text-blue-700 select-none"
-//         >
-//           Event Hub
-//         </NavLink>
-//       </div>
-
-//       <div className="navbar-center hidden lg:flex">
-//         <ul className="menu menu-horizontal px-1 gap-6">{links}</ul>
-//       </div>
-
-//       <div className="navbar-end flex items-center gap-3">
-//         {user ? (
-//           <>
-//             <div className="relative group inline-block">
-//               <img
-//                 src={user?.photoURL ? user?.photoURL : userPic}
-//                 alt="User"
-//                 referrerPolicy="no-referrer"
-//                 className="w-10 h-10 rounded-full cursor-pointer"
-//               />
-//               <span className="absolute bg-black bg-opacity-80 text-white text-sm rounded px-3 py-1 top-1/2 right-full -translate-y-1/2 mr-2 opacity-0 group-hover:opacity-100 transition duration-300 z-10 whitespace-nowrap">
-//                 {user?.name}
-//               </span>
-//             </div>
-
-//             <button
-//               onClick={handleLogout}
-//               className="btn btn-sm bg-blue-600 hover:bg-blue-700 text-white transition"
-//             >
-//               Log out
-//             </button>
-//           </>
-//         ) : (
-//           <>
-//             <NavLink
-//               to="/login"
-//               className="btn btn-sm bg-blue-600 hover:bg-blue-700 text-white transition"
-//             >
-//               Sign In
-//             </NavLink>
-//             {/* <NavLink
-//               to="/register"
-//               className="btn btn-sm bg-blue-600 hover:bg-blue-700 text-white transition"
-//             >
-//               Register
-//             </NavLink> */}
-//           </>
-//         )}
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
-
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { NavLink, useNavigate } from "react-router";
 import { AuthContext } from "../../Contexts/AuthContext";
 import Swal from "sweetalert2";
+import { FiLogOut } from "react-icons/fi";
+
 import { FaCalendarAlt } from "react-icons/fa";
 
 const Navbar = () => {
   const { user, setUser } = useContext(AuthContext);
   const navigate = useNavigate();
-  const userPic = "https://www.w3schools.com/howto/img_avatar.png";
+  const userPic = "https://www.w3schools.com/howto/img_avatar.png"; // Default user picture
+  const [imgSrc, setImgSrc] = useState(user?.photoURL || userPic);
+  useEffect(() => {
+    // Reset when user changes
+    setImgSrc(user?.photoURL || userPic);
+  }, [user]);
 
   const handleLogout = () => {
     Swal.fire({
@@ -181,7 +29,7 @@ const Navbar = () => {
       if (result.isConfirmed) {
         setUser(null);
         localStorage.removeItem("user");
-        navigate("/");
+        navigate("/login");
       }
     });
   };
@@ -279,26 +127,42 @@ const Navbar = () => {
 
       <div className="navbar-end flex items-center gap-4">
         {user ? (
-          <>
-            <div className="relative group inline-block">
-              <img
-                src={user?.photoURL || userPic}
-                alt="User"
-                referrerPolicy="no-referrer"
-                className="w-11 h-11 rounded-full cursor-pointer border-2 border-[#F6C26B] transition-shadow shadow-md"
-              />
-              <span className="absolute bg-[#2F855A] bg-opacity-90 text-[#FFF9F0] text-sm rounded px-3 py-1 top-1/2 right-full -translate-y-1/2 mr-2 opacity-0 group-hover:opacity-100 transition duration-300 z-10 whitespace-nowrap font-semibold font-body">
-                {user?.name}
-              </span>
-            </div>
-
-            <button
-              onClick={handleLogout}
-              className="btn btn-sm bg-[#2F855A] hover:bg-[#276e4f] text-[#FFF9F0] font-semibold shadow-md transition focus:outline-none focus:ring-4 focus:ring-[#F6C26B]"
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-11 h-11 border-[3px] border-[#F6C26B] rounded-full overflow-hidden shadow-md transition-transform duration-200 hover:scale-105">
+                <img
+                  src={imgSrc}
+                  alt="User"
+                  referrerPolicy="no-referrer"
+                  className="w-10 h-10 rounded-full cursor-pointer"
+                  onError={() => {
+                    if (imgSrc !== userPic) {
+                      setImgSrc(userPic); // fallback only if it's not already fallback
+                    }
+                  }}
+                />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="mt-3 p-3 shadow-xl dropdown-content bg-[#FFF9F0] rounded-xl w-60 z-50 border border-[#E2E8F0]"
             >
-              Log out
-            </button>
-          </>
+              <li className="mb-2">
+                <div className="text-sm text-center font-semibold text-[#2F855A] px-2 py-1 bg-[#A0DAB6] bg-opacity-20 rounded">
+                  👤 {user?.name}
+                </div>
+              </li>
+              <li>
+                <button
+                  onClick={handleLogout}
+                  className="w-full flex cursor-pointer items-center justify-between px-4 py-2 rounded-md text-[#E53E3E] hover:bg-[#FED7D7] font-medium transition-all"
+                >
+                  <span>Logout</span>
+                  <FiLogOut className="ml-2" />
+                </button>
+              </li>
+            </ul>
+          </div>
         ) : (
           <NavLink
             to="/login"
